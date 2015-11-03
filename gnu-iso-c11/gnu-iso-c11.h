f3e0a2e0e649d7d31f8a14d0d61a07173998f54f
macmade <macmade@xs-labs.com>
2015-11-03 21:13:54 +0100

    Initial project import...

---
+++ gnu-iso-c11/gnu-iso-c11.h
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jean-David Gadina - www-xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#ifndef GNU_ISO_C11_H
+#define GNU_ISO_C11_H
+
+#define gauto           auto
+#define gbreak          break
+#define gcase           case
+#define gchar           char
+#define gconst          const
+#define gcontinue       continue
+#define gdefault        default
+#define gdo             do
+#define gdouble         double
+#define gelse           else
+#define genum           enum
+#define gextern         extern
+#define gfloat          float
+#define gfor            for
+#define ggoto           goto
+#define gif             if
+#define ginline         inline
+#define gint            int
+#define glong           long
+#define gregister       register
+#define grestrict       restrict
+#define greturn         return
+#define gshort          short
+#define gsigned         signed
+#define gsizeof         sizeof
+#define gstatic         static
+#define gstruct         struct
+#define gswitch         switch
+#define gtypedef        typedef
+#define gunion          union
+#define gunsigned       unsigned
+#define gvoid           void
+#define gvolatile       volatile
+#define gwhile          while
+#define g_Alignas       _Alignas
+#define g_Alignof       _Alignof
+#define g_Atomic        _Atomic
+#define g_Bool          _Bool
+#define g_Complex       _Complex
+#define g_Generic       _Generic
+#define g_Imaginary     _Imaginary
+#define g_Noreturn      _Noreturn
+#define g_Static_assert _Static_assert
+#define g_Thread_local  _Thread_local
+
+#endif /* GNU_ISO_C11_H */
